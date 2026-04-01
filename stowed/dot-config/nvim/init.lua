993e1062c6afe2c3350a550bf0c1e1516e02878e
Javier Salcedo <javiersalcedo.p@gmail.com>
2026-04-01 21:05:38 +0100

    [nvim] Replace Lazy with vim.pack
    Requires Neovim >= 0.12

--- stowed/dot-config/nvim/init.lua
+++ stowed/dot-config/nvim/init.lua
@@ -51,6 +51,15 @@
     vim.o.cursorline  = true
     vim.o.winborder   = "rounded" -- For floating windows
 
+    vim.diagnostic.config(
+    {
+        virtual_lines =
+        {
+            severity = { min = vim.diagnostic.severity.ERROR } -- TODO: Make this toggleable
+        }
+        -- Use the default config for everything else
+    })
+
 
 -- AUTOCOMMANDS ------------------------------------------------------------------------------------
     -- Don't display the line numbers in terminal mode
@@ -64,20 +73,6 @@
                 vim.opt.relativenumber = false
             end
         } )
-
-    -- Set up the LSP when opening a file associated with it
-    vim.api.nvim_create_autocmd(
-        'LspAttach',
-        {
-            callback = function( event )
-                local client = vim.lsp.get_client_by_id( event.data.client_id )
-
-                if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
-                    vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-                    vim.lsp.completion.enable( true, client.id, event.buf, { autotrigger = true } )
-                end
-            end,
-        })
 
     -- Apply my custom theme tweaks after loading a different colorscheme
     -- TODO: Take into account light/dark backgrounds
@@ -120,18 +115,6 @@
             vim.fn.setreg( "+", path )
         end,
         {})
-
-    -- Restart LSP
-    vim.api.nvim_create_user_command(
-        "LSPRestart",
-        function ()
-            vim.cmd( 'lsp stop' )
-            vim.cmd.edit()
-        end,
-        {})
-
-    -- Print the currently attached LSPs
-    vim.api.nvim_create_user_command( "LspInfo", function() vim.cmd( 'checkhealth vim.lsp' ) end, {} )
 
 
 -- GENERAL FUNCTIONALITY --------------------------------------------------------------------------
@@ -186,18 +169,6 @@
     vim.keymap.set( 'n', 'tk', 'gt' )
     vim.keymap.set( 'n', 'tj', 'gT' )
 
-    -- vim.keymap.set( 'n', 'fo', 'zo', { desc = '[O]pen the [F]old under cursor' })
-    -- vim.keymap.set( 'n', 'fO', 'zR', { desc = '[O]pen all [F]olds' })
-    -- vim.keymap.set( 'n', 'fc', 'zc', { desc = '[C]lose the [F]old under cursor' })
-    -- vim.keymap.set( 'n', 'fC', 'zM', { desc = '[C]lose all [F]olds' })
-    -- vim.keymap.set( 'n', 'ft', 'za', { desc = '[T]oggle the [F]old under cursor' })
-
-    -- Go to definition
-    vim.keymap.set( 'n', 'gd', ':lua vim.lsp.buf.definition()<CR>', { desc = '[G]o to [D]efinition' })
-
-    -- Rename symbol under the cursor
-    vim.keymap.set( 'n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>', { desc = '[R]e[n]ame' })
-
     -- Shortcut to config
     vim.keymap.set( 'n', '<Leader>oc', ':e $MYVIMRC<CR>', { desc = '[O]pen the [C]onfig file' })
 
@@ -225,211 +196,141 @@
 
 
 -- LSP ---------------------------------------------------------------------------------------------
-    vim.diagnostic.config(
+    vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig' })
+    vim.lsp.enable(
     {
-        virtual_lines =
-        {
-            severity = { min = vim.diagnostic.severity.ERROR } -- TODO: Make this toggleable
-        }
-        -- Use the default config for everything else
+        'lua_ls',
+        'clangd',
+        'slangd'
     })
 
-    vim.lsp.enable( 'lua_ls' )
-    vim.lsp.enable( 'clangd' )
-    vim.lsp.enable( 'slangd' )
+    -- Set up the LSP when opening a file associated with it
+    vim.api.nvim_create_autocmd(
+        'LspAttach',
+        {
+            callback = function( event )
+                local client = vim.lsp.get_client_by_id( event.data.client_id )
+
+                if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+                    vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+                    vim.lsp.completion.enable( true, client.id, event.buf, { autotrigger = true } )
+                end
+            end,
+        })
+
+    -- Restart LSP
+    vim.api.nvim_create_user_command(
+        "LspRestart",
+        function ()
+            vim.cmd( 'lsp stop' )
+            vim.cmd.edit()
+        end,
+        {})
+
+    -- Print the currently attached LSPs
+    vim.api.nvim_create_user_command( "LspInfo", function() vim.cmd( 'checkhealth vim.lsp' ) end, {} )
+    -- Go to definition
+    vim.keymap.set( 'n', 'gd', ':lua vim.lsp.buf.definition()<CR>', { desc = '[G]o to [D]efinition' })
+    -- Rename symbol under the cursor
+    vim.keymap.set( 'n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>', { desc = '[R]e[n]ame' })
 
 
 -- PLUGINS -----------------------------------------------------------------------------------------
-    -- Bootstrap lazy.nvim
-    local lazypath = vim.fn.stdpath( "data" ) .. "/lazy/lazy.nvim"
-    if not ( vim.uv or vim.loop ).fs_stat( lazypath ) then
-        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-        if vim.v.shell_error ~= 0 then
-            vim.api.nvim_echo({
-                { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-                { out, "WarningMsg" },
-                { "\nPress any key to exit..." },
-            }, true, {})
-            vim.fn.getchar()
-            os.exit(1)
-        end
-    end
-    vim.opt.rtp:prepend( lazypath )
-    require( "lazy" ).setup(
+    vim.api.nvim_create_user_command( "PackInfo",   function() vim.pack.update( nil, {  offline = true }) end, {} )
+    vim.api.nvim_create_user_command( "PackUpdate", function() vim.pack.update() end, {} )
+    vim.api.nvim_create_user_command( "PackClean",
+        function()
+            vim.pack.del(
+                vim.iter(vim.pack.get())
+                    :filter(function(x) return not x.active end)
+                    :map(function(x) return x.spec.name end)
+                    :totable() )
+        end,
+        {})
+
+    vim.pack.add({
+        'https://github.com/dominikduda/vim_current_word',  -- Highlights instances of the word under cursor
+        'https://github.com/mg979/vim-visual-multi',        -- Multiple cursors using <C-n>
+        'https://github.com/echasnovski/mini.nvim',         -- Collection of various small independent plugins/modules
+        'https://github.com/folke/which-key.nvim',          -- Useful plugin to show you pending keybinds
+    })
+
+    -- Easymotion
+    vim.pack.add({ 'https://github.com/asvetliakov/vim-easymotion' })
+    vim.g.EasyMotion_smartcase = true
+    vim.keymap.set('n', '<Leader>j', '<Plug>(easymotion-overwin-f)', {desc = '[J]ump to character'})
+
+    -- Version control stuff
+    vim.pack.add({ 'https://github.com/mhinz/vim-signify' })
+    vim.g.signify_sign_change = '~'
+    vim.keymap.set('n', '<Leader>vd', ':SignifyHunkDiff<CR>', {desc = '[V]ersion Control [D]iff'})
+    vim.keymap.set('n', '<Leader>vu', ':SignifyHunkUndo<CR>', {desc = '[V]ersion Control [U]ndo'})
+
+    -- Simple statusline.
+    require( 'mini.statusline' ).setup({ use_icons = vim.g.have_nerd_font })
+
+    -- Better autocomplete than the native one
+    require( 'mini.completion' ).setup({})
+    -- Use Tab to navigate the suggestions
+    vim.keymap.set( 'i', '<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]],   { expr = true } )
+    vim.keymap.set( 'i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true } )
+
+    -- Better Around/Inside textobjects
+    require( 'mini.ai' ).setup { n_lines = 500 }
+
+    -- Add/delete/replace surroundings (brackets, quotes, etc.)
+    require( 'mini.surround' ).setup(
     {
-        spec = -- Plugins go here
+        mappings =
         {
-            'dominikduda/vim_current_word', -- Highlights instances of the word under cursor
-            'mg979/vim-visual-multi',       -- Multiple cursors using <C-n>
-            'neovim/nvim-lspconfig',
-            -- Version control stuff
+            add         = '<C-s>a', -- Add surrounding in Normal and Visual modes
+            delete      = '<C-s>d', -- Delete surrounding
+            replace     = '<C-s>r', -- Replace surrounding
+            find        = '',       -- Find surrounding (to the right)
+            find_left   = '',       -- Find surrounding (to the left)
+            highlight   = '',       -- Highlight surrounding
+
+            suffix_last = 'l',      -- Suffix to search with "prev" method
+            suffix_next = 'n',      -- Suffix to search with "next" method
+        }
+    })
+
+    -- Fuzzy Finder (files, lsp, etc)
+    vim.pack.add({
+        'https://github.com/nvim-telescope/telescope.nvim',
+        'https://github.com/nvim-lua/plenary.nvim',
+        'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+        'https://github.com/nvim-telescope/telescope-ui-select.nvim',
+    })
+    if vim.g.have_nerd_font == true then
+        vim.pack.add({ 'https://github.com/nvim-tree/nvim-web-devicons' })
+    end
+        require('telescope').setup
+        {
+            extensions =
             {
-                'mhinz/vim-signify',
-                config = function()
-                    vim.g.signify_sign_change = '~'
-                    vim.keymap.set('n', '<Leader>vd', ':SignifyHunkDiff<CR>', {desc = '[V]ersion Control [D]iff'})
-                    vim.keymap.set('n', '<Leader>vu', ':SignifyHunkUndo<CR>', {desc = '[V]ersion Control [U]ndo'})
-                end
+                ['ui-select'] = { require( 'telescope.themes' ).get_dropdown(), },
             },
-            -- Easymotion
-            {
-                'asvetliakov/vim-easymotion',
-                config = function()
-                    vim.g.EasyMotion_smartcase = true
-                    vim.keymap.set('n', '<Leader>j', '<Plug>(easymotion-overwin-f)', {desc = '[J]ump to character'})
-                end,
-            },
-            -- Collection of various small independent plugins/modules
-            {
-                'echasnovski/mini.nvim',
-                config = function()
-                    -- Better autocomplete than the native one
-                    require( 'mini.completion' ).setup({})
-                    -- Use Tab to navigate the suggestions
-                    vim.keymap.set( 'i', '<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]],   { expr = true } )
-                    vim.keymap.set( 'i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true } )
+        }
+        -- Enable Telescope extensions if they are installed
+        pcall( require( 'telescope' ).load_extension, 'fzf' )
+        pcall( require( 'telescope' ).load_extension, 'ui-select' )
 
-                    -- Better Around/Inside textobjects
-                    require( 'mini.ai' ).setup { n_lines = 500 }
+        local telescope_builtin = require 'telescope.builtin'
+        vim.keymap.set( 'n', '<leader>sh', telescope_builtin.help_tags,                     { desc = '[S]earch [H]elp' })
+        vim.keymap.set( 'n', '<leader>sk', telescope_builtin.keymaps,                       { desc = '[S]earch [K]eymaps' })
+        vim.keymap.set( 'n', '<leader>f',  telescope_builtin.find_files,                    { desc = 'Search [F]iles' })
+        vim.keymap.set( 'n', '<leader>ss', telescope_builtin.builtin,                       { desc = '[S]earch [S]elect Telescope' })
+        vim.keymap.set( 'n', '<leader>sw', telescope_builtin.grep_string,                   { desc = '[S]earch current [W]ord' })
+        vim.keymap.set( 'n', '<leader>sg', telescope_builtin.live_grep,                     { desc = '[S]earch by [G]rep' })
+        vim.keymap.set( 'n', '<leader>sd', telescope_builtin.diagnostics,                   { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set( 'n', 'gr',         telescope_builtin.lsp_references,                { desc = '[G]o to [R]eferences' })
+        vim.keymap.set( 'n', 'gsd',        telescope_builtin.lsp_document_symbols,          { desc = '[G]o to [S]ymbols in [D]ocument ' })
+        vim.keymap.set( 'n', 'gsw',        telescope_builtin.lsp_dynamic_workspace_symbols, { desc = '[G]o to [S]ymbols in [W]orkspace ' })
 
-                    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-                    require( 'mini.surround' ).setup(
-                    {
-                        mappings =
-                        {
-                            add         = '<C-s>a', -- Add surrounding in Normal and Visual modes
-                            delete      = '<C-s>d', -- Delete surrounding
-                            replace     = '<C-s>r', -- Replace surrounding
-                            find        = '',       -- Find surrounding (to the right)
-                            find_left   = '',       -- Find surrounding (to the left)
-                            highlight   = '',       -- Highlight surrounding
-
-                            suffix_last = 'l',      -- Suffix to search with "prev" method
-                            suffix_next = 'n',      -- Suffix to search with "next" method
-                        }
-                    })
-
-                    -- Simple statusline.
-                    require( 'mini.statusline' ).setup(
-                    {
-                        use_icons = vim.g.have_nerd_font
-                        -- Set the section for cursor location to l:LINE c:COLUMN
-                        --section_location = function()
-                            --return 'l:%4l c:%3v'
-                        --end
-                    })
-                end,
-            },
-            { -- Useful plugin to show you pending keybinds.
-                'folke/which-key.nvim',
-                event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-                opts =
-                {
-                    -- delay between pressing a key and opening which-key (milliseconds)
-                    -- this setting is independent of vim.o.timeoutlen
-                    delay = 0,
-                    icons =
-                    {
-                        -- set icon mappings to true if you have a Nerd Font
-                        mappings = vim.g.have_nerd_font,
-                        -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-                        -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-                        keys = vim.g.have_nerd_font and {} or
-                        {
-                            Up = '<Up> ',
-                            Down = '<Down> ',
-                            Left = '<Left> ',
-                            Right = '<Right> ',
-                            C = '<C-…> ',
-                            M = '<M-…> ',
-                            D = '<D-…> ',
-                            S = '<S-…> ',
-                            CR = '<CR> ',
-                            Esc = '<Esc> ',
-                            ScrollWheelDown = '<ScrollWheelDown> ',
-                            ScrollWheelUp = '<ScrollWheelUp> ',
-                            NL = '<NL> ',
-                            BS = '<BS> ',
-                            Space = '<Space> ',
-                            Tab = '<Tab> ',
-                            F1 = '<F1>',
-                            F2 = '<F2>',
-                            F3 = '<F3>',
-                            F4 = '<F4>',
-                            F5 = '<F5>',
-                            F6 = '<F6>',
-                            F7 = '<F7>',
-                            F8 = '<F8>',
-                            F9 = '<F9>',
-                            F10 = '<F10>',
-                            F11 = '<F11>',
-                            F12 = '<F12>',
-                        },
-                    },
-                    spec = {}, -- Name groups
-                },
-            },
-            { -- Fuzzy Finder (files, lsp, etc)
-                'nvim-telescope/telescope.nvim',
-                event = 'VimEnter',
-                dependencies =
-                {
-                    'nvim-lua/plenary.nvim',
-                    {
-                        'nvim-telescope/telescope-fzf-native.nvim',
-                        build = 'make',
-                        cond = function()
-                          return vim.fn.executable 'make' == 1
-                        end,
-                    },
-                    { 'nvim-telescope/telescope-ui-select.nvim' },
-                    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-                },
-                config = function()
-                    require('telescope').setup
-                    {
-                        extensions =
-                        {
-                            ['ui-select'] = { require( 'telescope.themes' ).get_dropdown(), },
-                        },
-                    }
-
-                    -- Enable Telescope extensions if they are installed
-                    pcall( require( 'telescope' ).load_extension, 'fzf' )
-                    pcall( require( 'telescope' ).load_extension, 'ui-select' )
-
-                    local builtin = require 'telescope.builtin'
-                    vim.keymap.set( 'n', '<leader>sh', builtin.help_tags,                     { desc = '[S]earch [H]elp' })
-                    vim.keymap.set( 'n', '<leader>sk', builtin.keymaps,                       { desc = '[S]earch [K]eymaps' })
-                    vim.keymap.set( 'n', '<leader>f',  builtin.find_files,                    { desc = 'Search [F]iles' })
-                    vim.keymap.set( 'n', '<leader>ss', builtin.builtin,                       { desc = '[S]earch [S]elect Telescope' })
-                    vim.keymap.set( 'n', '<leader>sw', builtin.grep_string,                   { desc = '[S]earch current [W]ord' })
-                    vim.keymap.set( 'n', '<leader>sg', builtin.live_grep,                     { desc = '[S]earch by [G]rep' })
-                    vim.keymap.set( 'n', '<leader>sd', builtin.diagnostics,                   { desc = '[S]earch [D]iagnostics' })
-                    vim.keymap.set( 'n', 'gr',         builtin.lsp_references,                { desc = '[G]o to [R]eferences' })
-                    vim.keymap.set( 'n', 'gsd',        builtin.lsp_document_symbols,          { desc = '[G]o to [S]ymbols in [D]ocument ' })
-                    vim.keymap.set( 'n', 'gsw',        builtin.lsp_dynamic_workspace_symbols, { desc = '[G]o to [S]ymbols in [W]orkspace ' })
-
-                    -- Shortcut for searching your Neovim configuration files
-                    vim.keymap.set('n', '<leader>sn', function()
-                        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-                    end,{ desc = '[S]earch [N]eovim files' })
-                end,
-              },
-            -- FIXME: Very slow?
-            -- Highlight, edit, and navigate code
-            --{
-            --    'nvim-treesitter/nvim-treesitter',
-            --    build   = ':TSUpdate',
-            --    main    = 'nvim-treesitter.configs', -- Sets main module to use for opts
-            --    opts    =
-            --    {
-            --        auto_install = true,
-            --        highlight    = { enable = true },
-            --    },
-            --},
-            --'nvim-treesitter/nvim-treesitter-context', -- Sticky scrolling
-        },
-        checker = { enabled = false } -- Automatic update checker
-    });
+        -- Shortcut for searching your Neovim configuration files
+        vim.keymap.set('n', '<leader>sn',
+            function()
+                telescope_builtin.find_files { cwd = vim.fn.stdpath 'config' }
+            end,
+            { desc = '[S]earch [N]eovim files' })
