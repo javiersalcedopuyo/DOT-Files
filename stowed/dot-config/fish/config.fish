8b9ae0b1cbc4ec71e5fea3fc0cb50ef309abfa95
Javier Salcedo <javiersalcedo.p@gmail.com>
2026-04-24 23:10:25 +0100

    [fish] Update config

--- stowed/dot-config/fish/config.fish
+++ stowed/dot-config/fish/config.fish
@@ -7,3 +7,10 @@ end
 function batman
     man $argv | bat --plain --language man
 end
+
+set -gx PATH $PATH /opt/homebrew/opt/llvm/bin
+
+set -gx RBENV_ROOT $HOME/.rbenv
+set -gx PATH $RBENV_ROOT/shims $PATH
+status --is-interactive; and source (rbenv init - | psub)
+
